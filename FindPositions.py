7cf784dcc3417bca53b62748d4e20f966723654c
hmkthor <hmkthor@gmail.com>
2022-04-07 23:18:17 +0900

    To feel the beuty of the binary search in a sorted array :)

--- FindPositions.py
+++ FindPositions.py
@@ -11,22 +11,30 @@ For optimal solution to this problem,
 from typing import List
 
 class Solution:
+
+    def searchRange(self, l: List[int], target: int) -> List[int]:
+        left = self.findFirstPosition(l, target)
+        right = self.findLastPosition(l, target)
+
+        return [left,right]
+
     def findFirstPosition(self, l: List[int], target: int) -> List[int]:
 
         left = 0
         right = len(l)-1
 
         while(left <= right):
-            mid = left + right // 2
+            mid = (left + right) // 2
 
             if(l[mid]== target):
-                if(mid==0 or l[mid-1]!=target):
+                if(mid==0 or (mid>=1 and l[mid-1]!=target)):
                     return mid
                 right = mid -1 
             elif(l[mid]>target):
                 right = mid -1
             else: 
                 left = mid + 1
+        return -1
 
     def findLastPosition(self, l: List[int], target: int) -> List[int]:
 
@@ -34,13 +42,19 @@ class Solution:
         right = len(l)-1
 
         while(left <= right):
-            mid = left + right // 2
+            mid = (left + right) // 2
 
             if(l[mid]== target):
-                if(mid==(len(l)-1) or l[mid+1]!=target):
+                if(mid==(len(l)-1) or (mid>=-1 and l[mid+1]!=target)):
                     return mid
                 left = mid + 1 
             elif(l[mid]>target):
                 right = mid -1
             else: 
                 left = mid + 1
+        return -1
+
+
+s = Solution()
+answer = s.searchRange([9,10,11,11,11,13,14,15,15,15,15], 15)
+print(answer)
