3d556744c673fc74832759cd1dd05c7d5943113c
hmkthor <hmkthor@gmail.com>
2022-04-18 23:45:51 +0900

    겹치는 문자열이 있을 경우

--- MinimumWindowSubstring.py
+++ MinimumWindowSubstring.py
@@ -17,6 +17,7 @@ ANSWER = BANC (4자)
 """
 
 class Solution:
+    # T 문자열이 서로 다를 때, 
     def findMWS(self, s:str, t:str) -> str:
         m = dict()
 
@@ -39,8 +40,52 @@ class Solution:
 
         return min_str
 
+    # 문자열이 서로 같을 때, 
+    def findMWS2(self, s:str, t:str) -> str:
+        n = len(s)
+        left, right = 0, 0
+        t_dict, s_dict = dict(), dict()
+        count = n
+        window = ""
+
+        for char in t:
+            if char not in t_dict:
+                t_dict[char] = 0
+            t_dict[char] += 1
+        
+        print(t_dict)
+
+        while left <= n-1 and right <= n-1 and left <= right:
+            if s[right] not in s_dict:
+                s_dict[s[right]] = 0
+            if s[right] not in t_dict:
+                s_dict[s[right]] += 1
+                right +=1
+                continue
+            else:
+               # 있으면 
+                s_dict[s[right]] += 1
+                # s_dict >= t_dict 이면, 
+                if all(item in s_dict.items() for item in t_dict.items()):
+                    if right - left +1 < count:
+                        count = right - left + 1
+                        window = s[left:right+1]
+                        print(window)
+                # 아니면, 
+                else:
+                    if s_dict[s[right]] > t_dict[s[right]]:
+                        s_dict[s[left]] -= 1
+                        left +=1
+            right += 1
+                
+            
+      
+        return window
+
+
 s = Solution()
 answer = s.findMWS("ADOBECODEBANCC","ABC")
+answer = s.findMWS2("ADOBECODDEBANCC","DDE")
 print(answer)                
 
        
